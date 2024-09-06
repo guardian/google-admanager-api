43cfca48fd8c6a3ae1a4808c819f807ec3942fce
Jake Lee Kennedy <jlkennedy127@gmail.com>
2024-09-06 16:51:11 +0100

    Add more authentication options

---
+++ lib/auth/googleAccessTokenCredential.ts
@@ -0,0 +1,13 @@
+import { Credential } from "./interfaces";
+
+export class GoogleAccessTokenCredential implements Credential {
+  private token: string;
+
+  constructor(access_token: string) {
+    this.token = access_token;
+  }
+
+  async getToken() {
+    return this.token;
+  }
+}
---
+++ lib/auth/googleRefreshTokenCredential.ts
@@ -0,0 +1,20 @@
+import { OAuth2Client } from "google-auth-library";
+import { Credential } from "./interfaces";
+
+export class GoogleRefreshTokenCredential implements Credential {
+  private client: OAuth2Client;
+
+  constructor(client_id: string, client_secret: string, refresh_token: string) {
+    this.client = new OAuth2Client(client_id, client_secret);
+
+    this.client.setCredentials({
+      refresh_token,
+    });
+  }
+
+  async getToken() {
+    const { token } = await this.client.getAccessToken();
+
+    return token;
+  }
+}
