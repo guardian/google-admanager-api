dfa236534c731486fdbbcad210255a8f8ec9c7f5
Demetrios <dskamiotis@gmail.com>
2025-06-18 10:21:50 +0100

    adds createTargetingPresets method to exposed TargetingPresetService class

---
+++ .changeset/three-fans-smash.md
@@ -0,0 +1,5 @@
+---
+"@guardian/google-admanager-api": patch
+---
+
+Updates the TargetingPresetService class by including the createTargetingPresets method
--- lib/client/services/targetingPresent/targetingPresent.type.ts
+++ lib/client/services/targetingPresent/targetingPresent.type.ts
@@ -1,4 +1,5 @@
 import type { PageResult } from "../../../common/types";
+import { Targeting } from "../../common/types";
 
 /**
  * Represents targeted or excluded ad units.
@@ -12,6 +13,21 @@ export type AdUnitTargeting = {
    * Whether or not all descendants are included (or excluded) as part of including (or excluding) this ad unit. By default, the value is true which means targeting this ad unit will target all of its descendants.
    */
   includeDescendants: boolean;
+};
+
+export type TargetingPreset = {
+  id: number;
+  /**
+   * The unique ID of the TargetingPreset. This value is readonly and is assigned by Google.
+   */
+  name: string;
+  /**
+   * The name of the TargetingPreset. This value is required to create a targeting preset and has a maximum length of 255 characters.
+   */
+  targeting: Targeting;
+  /**
+   * Contains the targeting criteria for the TargetingPreset. This attribute is required.
+   */
 };
 
 /**
