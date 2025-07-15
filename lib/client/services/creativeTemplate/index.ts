ba604ee0ba4697e6e45663f19b5e2c83e4c28d45
Demetrios <dskamiotis@gmail.com>
2025-07-15 11:22:56 +0100

    update LineItemStruct  exports

--- lib/client/services/creativeTemplate/index.ts
+++ lib/client/services/creativeTemplate/index.ts
@@ -1,3 +1,6 @@
 export { CreativeTemplateService } from "./creativeTemplate.service";
 export type * from "./creativeTemplate.type";
-export { CreativeTemplateType, CreativeTemplateStatus } from "./creativeTemplate.enum";
+export {
+  CreativeTemplateType,
+  CreativeTemplateStatus,
+} from "./creativeTemplate.enum";
